ea8be0a993077774b522c0873ffdbe039560c1dd
jfmartingomez-del <jfmartingomez@uma.es>
2026-09-21 11:01:32 +0200

    Delete COEFFICIENT OF VARIATION.R

--- COEFFICIENT OF VARIATION.R
+++
@@ -1,8 +0,0 @@
-#COEFFICIENT OF VARIATION
-standard_deviation <- function(arr) {
-  n <- length(arr)
-  std_dev <- sqrt(var(arr) * (n - 1) / n)
-  return(std_dev)
-}
-
-cv <- standard_deviation/abs(mean(arr))
