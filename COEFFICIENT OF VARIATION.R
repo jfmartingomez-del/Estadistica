9326bd29dc7ee121935e763c63969a6b2579802b
antoniolopezdelbarrio <antoniolopezdelbarrio@uma.es>
2026-09-21 10:54:38 +0200

    Add files via upload

---
+++ COEFFICIENT OF VARIATION.R
@@ -0,0 +1,8 @@
+#COEFFICIENT OF VARIATION
+standard_deviation <- function(arr) {
+  n <- length(arr)
+  std_dev <- sqrt(var(arr) * (n - 1) / n)
+  return(std_dev)
+}
+
+cv <- standard_deviation/abs(mean(arr))
